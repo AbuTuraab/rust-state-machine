47a2c3ee22a4aaabeeb608131bcd22989b801d3f
AbuTuraab <aliyulekan@gmail.com>
2025-01-07 14:15:42 +0100

    modify-dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use system::Config;
-
 mod balances;
 mod support;
 mod system;
 
 use crate::support::Dispatch;
+
 // These are the concrete types we will use in our simple state machine.
 // Modules are configured for these types directly, and they satisfy all of our
 // trait requirements.
 mod types {
-
 	pub type AccountId = String;
 	pub type Balance = u128;
 	pub type BlockNumber = u32;
@@ -19,32 +17,30 @@ mod types {
 	pub type Block = crate::support::Block<Header, Extrinsic>;
 }
 
+// These are all the calls which are exposed to the world.
+// Note that it is just an accumulation of the calls exposed by each module.
 pub enum RuntimeCall {
-	BalancesTransfer { to: types::AccountId, amount: types::Balance },
+	/* TODO: Turn this into a nested enum where variant `Balances` contains a `balances::Call`. */
+	Balances (balances::Call<Runtime>),
+	
 }
-
-/*
-	TODO:
-	Implement the `system::Config` trait you created on your `Runtime`.
-	Use `Self` to satisfy the generic parameter required for `system::Pallet`.
-*/
 
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 #[derive(Debug)]
 pub struct Runtime {
 	system: system::Pallet<Self>,
-
 	balances: balances::Pallet<Self>,
 }
 
-impl balances::Config for Runtime {
-	type Balance = types::Balance;
-}
 impl system::Config for Runtime {
 	type AccountId = types::AccountId;
 	type BlockNumber = types::BlockNumber;
 	type Nonce = types::Nonce;
+}
+
+impl balances::Config for Runtime {
+	type Balance = types::Balance;
 }
 
 impl Runtime {
@@ -53,18 +49,23 @@ impl Runtime {
 		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
 	}
 
+	// Execute a block of extrinsics. Increments the block number.
 	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
 		self.system.inc_block_number();
 		if block.header.block_number != self.system.block_number() {
-			return Err("Invalid block number");
+			return Err(&"block number does not match what is expected");
 		}
-		for support::Extrinsic { caller, call } in block.extrinsics.into_iter() {
+		// An extrinsic error is not enough to trigger the block to be invalid. We capture the
+		// result, and emit an error message if one is emitted.
+		for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
 			self.system.inc_nonce(&caller);
-			let res = self.dispatch(caller, call).map_err(|e| {
-				eprintln!("Extrinsic Error\n\tBlock {}\n\tError: {}", block.header.block_number, e)
+			let _res = self.dispatch(caller, call).map_err(|e| {
+				eprintln!(
+					"Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+					block.header.block_number, i, e
+				)
 			});
 		}
-
 		Ok(())
 	}
 }
@@ -72,63 +73,69 @@ impl Runtime {
 impl crate::support::Dispatch for Runtime {
 	type Caller = <Runtime as system::Config>::AccountId;
 	type Call = RuntimeCall;
-
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
 	fn dispatch(
 		&mut self,
 		caller: Self::Caller,
 		runtime_call: Self::Call,
 	) -> support::DispatchResult {
+		// This match statement will allow us to correctly route `RuntimeCall`s
+		// to the appropriate pallet level function.
 		match runtime_call {
-			RuntimeCall::BalancesTransfer { to, amount } => {
-				self.balances.transfer(caller, to, amount)?;
+			/*
+				TODO:
+				Adjust this logic to handle the nested enums, and simply call the `dispatch` logic
+				on the balances call, rather than the function directly.
+			*/
+			
+			RuntimeCall::Balances(call)=> {
+				self.balances.dispatch(
+					caller, call)?;
 			},
 		}
 		Ok(())
 	}
 }
 
-
-
 fn main() {
+	// Create a new instance of the Runtime.
+	// It will instantiate with it all the modules it uses.
 	let mut runtime = Runtime::new();
 	let alice = "alice".to_string();
 	let bob = "bob".to_string();
 	let charlie = "charlie".to_string();
 
+	// Initialize the system with some initial balance.
 	runtime.balances.set_balance(&alice, 100);
 
-	// // start emulating a block
-	// runtime.system.inc_block_number();
-	// assert_eq!(runtime.system.block_number(), 1);
-
-	// // first transaction
-	// runtime.system.inc_nonce(&alice);
-	// let _res = runtime
-	// 	.balances
-	// 	.transfer(alice.clone(), bob, 30)
-	// 	.map_err(|e| eprintln!("{}", e));
-
-	// // second transaction
-	// runtime.system.inc_nonce(&alice);
-	// let _res = runtime.
-	// balances.transfer(alice, charlie, 20)
-	// .map_err(|e| eprintln!("{}", e));
-
-
-	// start emulating a block
+	// Here are the extrinsics in our block.
+	// You can add or remove these based on the modules and calls you have set up.
 	let block_1 = types::Block {
-		header: types::Header {block_number:1},
-
+		header: support::Header { block_number: 1 },
 		extrinsics: vec![
+			/* TODO: Update your extrinsics to use the nested enum. */
+			//Balances (balances::Call<Runtime>),
+
+			
+			support::Extrinsic {
+				caller: alice.clone(),
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob, amount: 30 }),
+			},
 			support::Extrinsic {
 				caller: alice,
-				call: RuntimeCall::BalancesTransfer { 
-					to: bob, amount: 69 },
+				call: RuntimeCall::Balances(balances::Call::Transfer { to: charlie, amount: 20 }),
 			},
 		],
 	};
 
-	runtime.execute_block(block_1).expect("block execution failed");
+	// Execute the extrinsics which make up our block.
+	// If there are any errors, our system panics, since we should not execute invalid blocks.
+	runtime.execute_block(block_1).expect("invalid block");
 
+	// Simply print the debug format of our runtime state.
 	println!("{:#?}", runtime);
 }
